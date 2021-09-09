eab40516b11aaad81d8f756e734cc2746d5e2dc7
davleop <davleop1999@gmail.com>
2021-09-08 23:31:44 -0500

    fixed incorrect cd

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,12 +9,10 @@ if [ $(id -u) -ne 0 ]; then
   exit 1
 fi
 
-cd $HOME
-
 DEBIAN_FRONTEND=noninteractive
 
-apt update -y
-apt install -y software-properties-common
+sudo apt update -y
+sudo apt install -y software-properties-common
 add-apt-repository universe
 apt update -y
 
@@ -42,7 +40,6 @@ echo "export PATH=\$PATH:/usr/local/go/bin" >> /etc/profile
 
 wget https://download.oracle.com/otn-pub/java/jdk/16.0.2%2B7/d4a915d82b4c4fbb9bde534da945d746/jdk-16.0.2_linux-x64_bin.deb
 dpkg -i jdk-16.0.2_linux-x64_bin.deb
-
 
 # anything past here needs to be done for the regular user too
 curl https://pyenv.run | bash
