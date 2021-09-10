1de139ebfb0dd315d37408e9520d0dd654ba7db3
davleop <davleop1999@gmail.com>
2021-09-10 12:41:12 -0500

    fix download of jdk

--- bootstrap.sh
+++ bootstrap.sh
@@ -40,7 +40,7 @@ wget https://golang.org/dl/go1.17.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz
 echo 'export PATH="\$PATH:/usr/local/go/bin"' >> .profile
 
-wget https://download.oracle.com/otn-pub/java/jdk/16.0.2%2B7/d4a915d82b4c4fbb9bde534da945d746/jdk-16.0.2_linux-x64_bin.tar.gz
+wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/16.0.2%2B7/d4a915d82b4c4fbb9bde534da945d746/jdk-16.0.2_linux-x64_bin.tar.gz
 tar zxvf jdk-16.0.2_linux-x64_bin.tar.gz -C /opt
 echo 'export PATH="/opt/jdk-16.0.2/bin:$PATH"' >> .profile
 
